ff7f27befe6624a2f16e868b064dd99c3086282d
Wen-Wei Tseng <sosiristseng@gmail.com>
2022-03-22 16:28:09 +0800

    rename

--- scripts/find_solutions/main.jl
+++ scripts/find_solutions/main.jl
@@ -7,17 +7,17 @@ Pkg.status()
 Pkg.instantiate()
 
 #' Load library to main worker
-@time using Revise, Distributed, RetroSignalModel, CSV
+@time using Distributed, RetroSignalModel, CSV
 import RetroSignalModel as rs
 #' Add workers
 addprocs(exeflags="--project=$(Base.active_project())");
 @show nprocs();
 
 #' Load library to workers
-@time @everywhere using Revise, RetroSignalModel;
+@time @everywhere using RetroSignalModel;
 
 #' Parameter searching
-@time df = rs.paramSearching(rs.rtgM4(); num_sim=1e9, save_iter=1e6,distributed=true, saveall=false)
+@time df = rs.paramSearching(rs.rtgM4(); num_sim=1e9, save_iter=1e6, distributed=true, saveall=false)
 
-#'
+#'Output
 CSV.write("data/solution_rtgM4.csv", df)
