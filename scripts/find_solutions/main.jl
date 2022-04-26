0c81004057b62cb88cfbd3cbe0a62d6627f0f02c
Wen-Wei Tseng <sosiristseng@gmail.com>
2022-04-26 20:24:24 +0800

    fix ci error

--- scripts/find_solutions/main.jl
+++
@@ -1,9 +0,0 @@
-#' Load library to main worker
-using CSV
-import RetroSignalModel as rs
-
-#' Parameter searching
-@time df = rs.search_params(rs.rtgM4(); num_sim=1e9, save_iter=1e6, distributed=true, saveall=false)
-
-#'Output
-CSV.write("../../src/data/solution_rtgM4.csv", df)
