50a3f5720edf85fa639a56f1a100102b3da577c2
Wen Wei Tseng <sosiristseng@gmail.com>
2023-12-18 15:05:49 +0800

    self-hosted

--- execute.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-julia -e 'import IJulia; IJulia.installkernel("Julia", "--project=@.")'
-
-jupyter nbconvert --to notebook --execute --inplace ${EXTRA_ARGS} \
-    --ExecutePreprocessor.timeout=${TIMEOUT} \
-    --ExecutePreprocessor.kernel_name=$(julia -e 'print("julia-1.",VERSION.minor)') \
-    "docs/${NOTEBOOK}"
